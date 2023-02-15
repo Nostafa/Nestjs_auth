6b4f2aa117f445cd0776e7f46547defea9c4251b
Nostafa <mostafamok.mm@gmail.com>
2023-02-16 00:04:53 +0200

    start creating user module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  async signup() {
+    return { message: 'signup was successful' };
+  }
+  async login() {
+    return '';
+  }
+  async logout() {
+    return '';
+  }
+}
